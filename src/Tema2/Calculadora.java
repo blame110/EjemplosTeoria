8f5d9fa79594bb72fb2e31b6b3513b69be2e6c58
blame110 <46011863+blame110@users.noreply.github.com>
2020-11-18 09:43:50 +0100

    No prestar atención

--- src/Tema2/Calculadora.java
+++ src/Tema2/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
 
 		for (int i = 1; i <= 10; i++) {
 
-			System.out.println("\nTabla de Multiplicar del " + i + "\n");
+			System.out.println("\nTabla de Multiplicacion del " + i + "\n");
 
 			for (int j = 1; j <= 10; j++) {
 				System.out.println(i + " x " + j + " = " + i * j);
